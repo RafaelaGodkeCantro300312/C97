b6921704f9e5a2f9b4833452719aba1b0649c3df
RafaelaGodkeCantro300312 <113138692+RafaelaGodkeCantro300312@users.noreply.github.com>
2023-03-28 16:44:06 -0300

    Add files via upload

--- Bird_talkPage.js
+++ Bird_talkPage.js
@@ -47,7 +47,7 @@ function getData() {
         nameWithTag="<h4>"+name+"<img class='user_tick' src='tick.png'></h4>";
         messageWithTag="<h4 class='message_h4'>"+message+"</h4>";
         likeButton="<button class='btn btn-warning' id="+firebase_message_id+" value="+like+" onclick='updateLike(this.id)'>";
-        spanWithTag="<span class='glyphicon glyphicon-thumbs-up'>Like: "+like+"</span></button><hr>";
+        spanWithTag="<span class='glyphicon glyphicon-thumbs-up'> Like: "+like+"</span></button><hr>";
         row=nameWithTag+messageWithTag+likeButton+spanWithTag;
         document.getElementById("output").innerHTML+=row;
       }
